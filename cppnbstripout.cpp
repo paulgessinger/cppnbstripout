1d59ac4775f9165e284c1b99af67a84f83fd6933
Paul Gessinger <hello@paulgessinger.com>
2020-05-07 09:37:46 +0200

    fixes

--- cppnbstripout.cpp
+++ cppnbstripout.cpp
@@ -1,35 +1,78 @@
 #include <fstream>
 #include <iostream>
 #include <nlohmann/json.hpp>
+#include <argparse.hpp>
+
 
 using nlohmann::json;
 
-void clean(json& j) {
-  //j["metadata"]["language_info"]["version"] = nullptr;
+void clean(json& j, bool strip_kernel_spec) {
+  // j["metadata"]["language_info"]["version"] = nullptr;
 
   json& cells = j["cells"];
 
   for (auto& cell : cells) {
-    if(cell.find("execution_count") != cell.end()) {
-        cell["execution_count"] = nullptr;
+    if (cell.find("execution_count") != cell.end()) {
+      if (!cell["execution_count"].is_null()) {
+        if (cell["execution_count"].get<int>() != 0) {
+          cell["execution_count"] = nullptr;
+        }
+      }
     }
-    if(cell.find("outputs") != cell.end()) {
+    if (cell.find("outputs") != cell.end()) {
       cell["outputs"] = json::array();
     }
-    if(cell.find("metadata") != cell.end()) {
+    if (cell.find("metadata") != cell.end()) {
       cell["metadata"] = json::object();
     }
+  }
+
+  if(strip_kernel_spec) {
+    j["metadata"]["kernelspec"] = nullptr;
   }
 }
 
 int main(int argc, char* argv[]) {
-  if (argc > 1) {
-    std::string arg{argv[1]};
+  argparse::ArgumentParser program("cppnbstripout");
 
-    if (arg == "-h") {
-      std::cerr << "usage: cppnbstripout [INPUT] [OUTPUT]" << std::endl;
-      return 0;
-    }
+  program.add_argument("--kernel-spec").default_value(false).implicit_value(true);
+  program.add_argument("args").remaining();
+
+  try {
+    program.parse_args(argc, argv);
+  }
+  catch (const std::runtime_error& err) {
+    std::cout << err.what() << std::endl;
+    std::cout << program;
+    exit(0);
+  }
+
+  std::vector<std::string> args;
+  try {
+    args = program.get<std::vector<std::string>>("args");
+  } 
+  catch (const std::logic_error&) {
+  }
+
+  if(args.size() > 2) {
+    throw std::runtime_error("Too many arguments");
+  }
+
+  for(auto arg: args) { std::cout << arg << std::endl; }
+
+  bool strip_kernel_spec = program.get<bool>("--kernel-spec");
+  std::cout << strip_kernel_spec << std::endl;
+
+  std::string infile = "-";
+  std::string outfile = "-";
+    
+  if(args.size() == 2) {
+    infile = args[0];
+    outfile = args[1];
+  }
+
+  if(args.size() == 1) {
+    infile = args[0];
   }
 
   std::unique_ptr<std::istream> ifs{nullptr};
@@ -37,15 +80,6 @@ int main(int argc, char* argv[]) {
 
   std::istream* is{&std::cin};
   std::ostream* os{&std::cout};
-
-  std::string infile = "-";
-  std::string outfile = "-";
-  if (argc > 1) {
-    infile = argv[1];
-  }
-  if (argc > 2) {
-    outfile = argv[2];
-  }
 
   if (infile != "-") {
     ifs = std::make_unique<std::ifstream>(infile);
@@ -56,7 +90,7 @@ int main(int argc, char* argv[]) {
   json input;
   (*is) >> input;
 
-  clean(input);
+  clean(input, strip_kernel_spec);
 
   if (outfile == "") {
     // save to input file
--- test.sh
+++ test.sh
@@ -9,7 +9,10 @@ act2=act2.ipynb
 
 curl https://raw.githubusercontent.com/jupyter/notebook/master/docs/source/examples/Notebook/Running%20Code.ipynb > $raw
 
+echo "Test 1"
 cat $raw | ./cppnbstripout > $act1
 diff $ref $act1 > /dev/null
+echo "Test 2"
 ./cppnbstripout $raw > $act2
 diff $ref $act2 > /dev/null
+echo "All good"
